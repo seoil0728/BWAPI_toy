b383ee99db3de1c166032d73a74dd503b34c7375
seoil0728 <78630419+seoil0728@users.noreply.github.com>
2022-07-20 10:42:22 +0900

    make mmr folder, via view_mmr_order

--- view_mmr_order.py
+++ view_mmr_order.py
@@ -23,7 +23,7 @@ player_list.reverse()
 rank = 0
 
 tm = time.localtime(time.time())
-file_name = './' + str(tm.tm_year) + '_' + str(tm.tm_mon) + '_' + str(tm.tm_mday) + '_MMR Rank.txt'
+file_name = 'mmr/' + str(tm.tm_year) + '_' + str(tm.tm_mon) + '_' + str(tm.tm_mday) + '_MMR Rank.txt'
 with open(file_name, 'w') as f:
     for i in player_list:
         rank += 1
